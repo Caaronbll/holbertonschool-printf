9490393101d629a6cd456c3db767c3d41d130739
root <112408198+Night8Guy@users.noreply.github.com>
2023-03-08 13:32:59 -0800

    updated README

--- main.h
+++ main.h
@@ -3,7 +3,15 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/* prototypes */
+/* _printf - Recieves the main string and all parameters to print
+ *  a formatted string.
+ *  It returns an integer and takes a first parameter of a pointer to a constant character an
+ *  d a number of following parameters
+ *  If you happen to pass in more parameters than are required by the format string you pass
+ *  in, then the extra parameters are ignored
+ *
+ *
+ * */
 int _printf(const char *format, ...);
 
 #endif
