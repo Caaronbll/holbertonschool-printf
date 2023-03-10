133ce40a1eeebcb5d330710629d953ce0f9a4e0b
Caaronbll <aaronbll46@gmail.com>
2023-03-10 13:37:08 -0800

    test

--- main.h
+++ main.h
@@ -3,23 +3,18 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/* _printf - Recieves the main string and all parameters to print
- *  a formatted string.
- *  It returns an integer and takes a first parameter of a pointer to a constant character an
- *  d a number of following parameters
- *  If you happen to pass in more parameters than are required by the format string you pass
- *  in, then the extra parameters are ignored
- *
- *
- * */
+/* specstr - a structure used to hold the functions allocated to each symbol
+ * @symbol: symbol used as an argument
+ * @f: function pointer for each function allocated for each symbol
+ * Return: nothing
+ */
 
-struct spec_sym
+typedef struct specstr
 {
-	char *sym
+	char *symbol
 	int (*f)(va_list);
 };
 
-typedef struct spec_sym symbol;
 
 
 
